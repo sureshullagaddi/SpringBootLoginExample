36b50971b4001e54efb096d504dc8fe8699fce23
suresh <suresh@192.168.1.12>
2019-11-21 23:44:26 +0530

    Added home page

--- LoginExample/src/main/java/com/springboot/loginexample/LoginExample/LoginController.java
+++ LoginExample/src/main/java/com/springboot/loginexample/LoginExample/LoginController.java
@@ -4,25 +4,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springboot.loginexample.model.User;
 
 @Controller
-public class HomeController {
+public class LoginController {
 
 	@RequestMapping("/login")
 	public String index() {
 		return "login";
 	}
 
-	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	//@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public ModelAndView save(@ModelAttribute User user) {
-
+		
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("user-data");
 		modelAndView.addObject("user", user);
 		return modelAndView;
 	}
 	//https://www.thymeleaf.org/documentation.html
+	
+	@RequestMapping(value = "/home" ,method = RequestMethod.POST)
+	public String loginValidation(@RequestParam String name,@RequestParam String email) {
+		if(name != null && email != null)
+		return "Home";
+		return "login";
+	}
 }

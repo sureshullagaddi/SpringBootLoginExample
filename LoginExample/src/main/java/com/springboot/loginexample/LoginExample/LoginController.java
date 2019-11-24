d2e60b06218738fa25ef26c81edc13af87206856
suresh <suresh@192.168.1.12>
2019-11-24 19:25:10 +0530

    Added support for html to html link

--- LoginExample/src/main/java/com/springboot/loginexample/LoginExample/LoginController.java
+++ LoginExample/src/main/java/com/springboot/loginexample/LoginExample/LoginController.java
@@ -33,8 +33,8 @@ public class LoginController {
 		return "login";
 	}
 	
-//	@RequestMapping(value="/Fixedline",method = RequestMethod.GET)
-//	public String fixedLine() {
-//		return "Fixedline";
-//	}
+	@RequestMapping(value="/Fixedline",method = RequestMethod.GET)
+	public String fixedLine() {
+		return "Fixedline";
+	}
 }
